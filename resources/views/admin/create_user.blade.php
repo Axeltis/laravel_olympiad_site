00238d5918ab2cd63d8a6210d7702c78861e0ad8
Axeltis <79056843+Axeltis@users.noreply.github.com>
2021-06-28 16:26:05 +0400

    Update create_user.blade.php

--- resources/views/admin/create_user.blade.php
+++ resources/views/admin/create_user.blade.php
@@ -199,7 +199,7 @@
                                                             <option selected value="1">1</option>
                                                             <option value="2">2</option>
                                                             <option value="3">3</option>
-                                                            <option value="4">4</option>
+                                                         
                                                         </select>
                                                     </div>
                                                 </div>
@@ -268,17 +268,11 @@
 
                                                     <div class="col-md-6">
                                                         <select id="pupil_class" name="pupil_class" class="form-control">
-                                                            <option value="1">1</option>
-                                                            <option value="2">2</option>
-                                                            <option value="3">3</option>
-                                                            <option value="4">4</option>
-                                                            <option value="5">5</option>
-                                                            <option value="6">6</option>
+                                                    
+                                                            <option selected value="6">6</option>
                                                             <option value="7">7</option>
                                                             <option value="8">8</option>
-                                                            <option selected value="9">9</option>
-                                                            <option value="10">10</option>
-                                                            <option value="11">11</option>
+                                                 
                                                         </select>
                                                     </div>
                                                 </div>
