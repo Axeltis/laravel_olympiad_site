a567849334ff8a73eac789a8a9b9cd9853613336
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2021-07-12 18:00:54 +0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous">-->
-    <!-- Scripts -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- Scripts --><script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,7 +23,7 @@
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <style>
    
@@ -95,6 +95,85 @@ background-color: transparent;
             background-color: seagreen;
         }
         
+            .facts{
+        margin: 0 auto;
+        border-radius: 12px;
+        width:100%;
+        height:200px;
+        align-items: center;
+        background-color: transparent;
+        display: flex;
+        justify-content: center;
+        color: aliceblue;
+        font-size: 1.7rem;
+    }
+    .customLIbullets li::marker{
+        font-size: 1.7rem;
+        content: "»";
+        font-weight: 800;
+    }
+    li{
+        font-size: 1.65rem;
+        padding-left: 1.4em
+    }
+  .card-body  p{
+        text-indent: 1.5em;
+        font-size: 1.7rem
+    }
+    @media screen  and (max-width: 765px) {
+        .customLIbullets li::marker{
+        font-size: .81rem;
+        content: "»";
+        font-weight: 800;
+    }
+    li{
+        padding-left: 1.1em;
+        font-size: .81rem;
+    }
+  .card-body  p{
+        text-indent: 1.1em;
+        font-size: .91rem
+    }
+    .facts{
+        margin: 0 auto;
+        border-radius: 12px;
+        width:75%;
+        height:200px;
+        align-items: center;
+        background-color: transparent;
+        display: flex;
+        justify-content: center;
+        color: aliceblue;
+        font-size: .85rem;
+    }
+    }
+    @media screen  and (max-width: 1024px) and (min-width:765px) {
+        .customLIbullets li::marker{
+        font-size: 1.5rem;
+        content: "»";
+        font-weight: 800;
+    }
+    li{
+        font-size: 1.25rem;
+        padding-left: 1.2em
+    }
+  .card-body  p{
+        text-indent: 1.1em;
+        font-size: 1.3rem
+    }
+    .facts{
+        margin: 0 auto;
+        border-radius: 12px;
+        width:100%;
+        height:200px;
+        align-items: center;
+        background-color: transparent;
+        display: flex;
+        justify-content: center;
+        color: aliceblue;
+        font-size: 1.2rem;
+    }
+    }
            
     </style>
 
